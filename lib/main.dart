d08759cb4d0f79992c6be4eddd1797c42fffbe28
Chirag <andrag.apps@gmail.com>
2021-09-01 06:40:22 +1200

    FIRST COMMIT

---
+++ lib/main.dart
@@ -0,0 +1,124 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Andrag Apps',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        fontFamily: "LibreBaskerville-Regular",
+      ),
+      debugShowCheckedModeBanner: false,
+
+      ///Calling Another Widget
+      home: MyHomePage(title: 'Mi Card'),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  final String title;
+
+  const MyHomePage({Key key, this.title}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.teal,
+      body: SafeArea(
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Container(
+                decoration: BoxDecoration(
+                  border: Border.all(
+                    color: Colors.white,
+                    width: 3,
+                  ),
+                  shape: BoxShape.circle,
+                  color: Colors.teal,
+                ),
+                child: CircleAvatar(
+                  radius: 50.0,
+                  backgroundImage: AssetImage(
+                      "res/andrag_images/IMG_20190617_201952_222.jpg"),
+                ),
+              ),
+              Text(
+                "Andrag Apps",
+                style: TextStyle(
+                  fontFamily: "Pacifico-Regular",
+                  fontSize: 30.0,
+                  color: Colors.white,
+                ),
+              ),
+              SizedBox(height: 8.0),
+              Text(
+                "FLUTTER DEVELOPER",
+                style: TextStyle(
+                  fontFamily: "SourceSansPro-Regular",
+                  fontSize: 20.0,
+                  letterSpacing: 2.5,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.teal.shade100,
+                ),
+              ),
+              SizedBox(
+                height: 20.0,
+                width: 150.0,
+                child: Divider(
+                  color: Colors.teal.shade100,
+                ),
+              ),
+              Card(
+                color: Colors.white,
+                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                child: ListTile(
+                  leading: Icon(
+                    Icons.call,
+                    color: Colors.teal,
+                  ),
+                  title: Text(
+                    "+64-22-467-9419",
+                    style: TextStyle(
+                      fontFamily: "SourceSansPro-Regular",
+                      fontSize: 20.0,
+                      letterSpacing: 2.5,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.teal.shade900,
+                    ),
+                  ),
+                ),
+              ),
+              Card(
+                color: Colors.white,
+                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                child: ListTile(
+                  leading: Icon(
+                    Icons.email,
+                    color: Colors.teal,
+                  ),
+                  title: Text(
+                    "admin@andrag.co.nz",
+                    style: TextStyle(
+                      fontFamily: "SourceSansPro-Regular",
+                      fontSize: 20.0,
+                      letterSpacing: 2.5,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.teal.shade900,
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
